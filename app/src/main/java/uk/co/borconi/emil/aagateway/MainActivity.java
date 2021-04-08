212a5e7deb71d7d34a9dba3997efa203bf7094e5
Michal Hapala <mikee@mikee.cz>
2021-04-08 20:33:46 +0200

    find bt wip

--- app/src/main/java/uk/co/borconi/emil/aagateway/MainActivity.java
+++ app/src/main/java/uk/co/borconi/emil/aagateway/MainActivity.java
@@ -1,8 +1,13 @@
 package uk.co.borconi.emil.aagateway;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
@@ -13,21 +18,64 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.ToggleButton;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 
+import static android.app.NotificationManager.IMPORTANCE_HIGH;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
     private static final String TAG = "AAGateWay";
     private SharedPreferences preferences;
+    private IntentFilter filter;
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            Log.w(TAG, "ON_RECEIVE");
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.w(TAG, "FOUND");
+            }
+            else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                Log.w(TAG, "CONNECTED");
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.w(TAG, "FINISHED");
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+                Log.w(TAG, "DISCONNECT_REQ");
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                Log.w(TAG, "DISCONNECTED");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.aawifi)
+                .setContentTitle("Title")
+                .setContentText("Content");
+        Notification n = builder.build();
+        NotificationManager nm = (NotificationManager)this.getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(45468, n);
+
+        Log.w(TAG, "CREATING");
+
+        filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(mReceiver, filter);
 
         if (getIntent().getAction()!=null && getIntent().getAction().equalsIgnoreCase("android.intent.action.MAIN")) {
 
@@ -63,12 +111,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-
     }
 
     @Override
-        protected void onResume() {
+    protected  void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
+
+        registerReceiver(mReceiver, filter);
 
         Intent paramIntent = getIntent();
         Intent i = new Intent(this, HackerService.class);
